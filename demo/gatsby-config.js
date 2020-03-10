e0a46f2c619e7625f5cf65217427e29f644b3c1f
Ste Parratt <s.parratt@yahoo.com>
2020-03-10 17:15:11 +0000

    Changed description for gatsby-config

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -9,10 +9,9 @@ module.exports = {
     {
       resolve: "@rennehir/gatsby-theme-personal-site",
       options: {
-        siteTitle: "Renne Hirsimäki",
+        siteTitle: "Steven Parratt",
         siteDescription:
-          "This is an example site of gatsby-theme-personal-site",
-        siteUrl: "https://gatsby-theme-personal-site.netlify.com",
+          "Freelance Web Developer based in Liverpool, England. I produce ultra-performant web apps.",
         // Set your page's content's base paths
         blogBasePath: "/blog", // default: /blog
         portfolioBasePath: "/benefits", // default: /portfolio
